956407f5cbfbcd183010d530187828febdd4fc5f
mjsangjoon <michaeljchin@hotmail.com>
2016-04-25 02:00:15 -0700

    some message

--- src/edu/ucsd/cs110/temperature/Celsius.java
+++ src/edu/ucsd/cs110/temperature/Celsius.java
@@ -24,6 +24,6 @@ public class Celsius extends Temperature {
     public String toString()
         {
             // TODO: Complete this method
-            return this.getValue() + " c";
+            return this.getValue() + " C";
         }
 }
